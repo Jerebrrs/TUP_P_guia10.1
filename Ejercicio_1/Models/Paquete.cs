f2570b0c1bd49b2f260af88f1ef9eb119bea68d5
Kevin Jeremias Barrios <kevinbrrs96@gmail.com>
2025-10-30 20:04:59 -0300

    Avance de ejercicio 1

---
+++ Ejercicio_1/Models/Paquete.cs
@@ -0,0 +1,20 @@
+﻿namespace Guia_10._1.Models
+{
+    internal class Paquete
+    {
+        public int DNiRemitente { get; set; }
+        public string NombreRemitente { get; set; }
+        public string Direccion { get; set; }
+
+        public Paquete(int dni, string nombre, string direccion)
+        {
+            this.DNiRemitente = dni;
+            this.NombreRemitente = nombre;
+            this.Direccion = direccion;
+        }
+        public override string ToString()
+        {
+            return $@"{NombreRemitente}({DNiRemitente} - {Direccion})";
+        }
+    }
+}
---
+++ Ejercicio_1/Models/Repartidor.cs
@@ -0,0 +1,52 @@
+﻿namespace Guia_10._1.Models
+{
+    internal class Repartidor
+    {
+        public int Capacidad { get; }
+        public int Ocupacion { get; }
+
+        Stack<Paquete> entregas = new Stack<Paquete>();
+
+        public Repartidor(int capacidad)
+        {
+            this.Capacidad = capacidad;
+        }
+
+        public Paquete Revisar()
+        {
+            if (entregas.Count() >= 0)
+            {
+                return entregas.Peek();
+            }
+            return null;
+        }
+
+        public void Cargar(Paquete paq)
+        {
+            if (paq != null)
+            {
+                entregas.Push(paq);
+            }
+        }
+
+        public Paquete Descargar()
+        {
+            Paquete eliminado = null;
+            if (entregas.Count()>=0)
+            {
+               eliminado= entregas.Pop();
+                return eliminado;
+            }
+            return null;
+        }
+
+        public bool TieneCapacidadDisponible()
+        {
+            if (entregas.Count()< Capacidad)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
